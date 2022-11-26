04e3347c3c53d73d107c9d09cf37436c8d0d358a
StupidNut <42118851+StupidNut@users.noreply.github.com>
2022-11-26 19:57:41 +0800

    StopKnife metod
    Проверка попадания перенесена в мишень. Остановка ножа вызывается ивентом. Снижение скорости - метод у класса ножа.

--- Assets/Scripts/Knife.cs
+++ Assets/Scripts/Knife.cs
@@ -4,28 +4,38 @@ public class Knife : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody2D knifeRigidbody;
-    private bool attachedToWood;
+    [SerializeField] public LayerMask knifeMask;
+    private Collider2D knifeColaider;
+    public bool attachedToWood;
 
-    
+
+    private void Start()
+    {      
+        knifeColaider = GetComponent<Collider2D>();
+        Wood.HitMainTarget += HittingTarget;
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collisionObject)
     {
         if (collisionObject.GetComponent<Knife>()?.attachedToWood == true)
         {
+            knifeColaider.isTrigger = false;
+            attachedToWood = false;
+
             knifeRigidbody.velocity = Vector2.down+Vector2.right;            
             knifeRigidbody.gravityScale = 2;
             transform.parent = null;
-            attachedToWood = false;
 
-            Debug.LogError("Вы проиграли"); // need to be transferred to scene loader after implementation
-        }
-        
-        if (collisionObject.gameObject.layer == 6) // 6 - MainTarget layer
-        {
-            knifeRigidbody.velocity = Vector2.zero;
-            transform.parent = collisionObject.transform;
-            attachedToWood = true;            
-        }
-                
+            Debug.Log("Вы проиграли"); // need to be transferred to scene loader after implementation
+        }       
+    }
+
+    private void HittingTarget(Collider2D target)
+    {
+        knifeRigidbody.velocity = Vector2.zero;
+        transform.parent = target.transform;
+        attachedToWood = true;        
     }
 
 }
--- Assets/Scripts/Wood.cs
+++ Assets/Scripts/Wood.cs
@@ -1,13 +1,32 @@
 using UnityEngine;
+using System;
 
 public class Wood : MonoBehaviour
 {
 
-    [SerializeField] private float speed;    
-        
+    [SerializeField] private float speed;
+    [SerializeField] public LayerMask mainTargetMask;
+    private Collider2D woodColaider;
+
+    public static Action<Collider2D> HitMainTarget;
+
+    private void Start()
+    {
+        woodColaider = GetComponent<Collider2D>();        
+    }
+
     private void Update()
     {
         transform.Rotate(0, 0, speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collisionObject)
+    {
+        if (collisionObject.gameObject.layer == (collisionObject.gameObject.layer | ( 1 << mainTargetMask))) //LayerMask.NameToLayer("MainTarget") ) // 7 - KnifeMask layer ((layerMask.value & (1 << collider.transform.gameObject.layer)) > 0)
+        {
+            HitMainTarget?.Invoke(woodColaider);           
+        }
+
     }
 
 }
